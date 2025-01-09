4ffe00b89b22aa089838b4c5b9201f8058eb6a3c
sh09unn3r <marcusquach07@gmail.com>
2025-01-09 13:32:01 -0700

    Done

--- calculator.py
+++ calculator.py
@@ -1,3 +1,48 @@
+# Creates a variable called binary_to_decimal that takes in a binary number and converts it to decimal
+def binary_to_decimal(binary):
+
+    # Checks if length is valid for a 8 bit binary number and if all bits are either 0 or 1
+    if len(binary) != 8 or not all(bit in ['0', '1'] for bit in binary):
+        # Returns error message if binary number is not valid
+        return "Error"
+    # Argument to store decimal number, set as 0
+    decimal = 0
+        
+    # For loop for i in a range of 0-7
+    for i in range(8):
+         # Equation for binary to decimal conversion
+        decimal += int(binary[i]) * (2 ** (7 - i))
+    # Returns decimal number
+    return decimal
+
+# Creates a variable called decimal_to_binary that takes in a decimal number and converts it to binary
+def decimal_to_binary(decimal):
+
+    # Checks if decimal is smaller than 0 or greater than 255
+    if decimal < 0 or decimal > 255:
+        # Returns error message if decimal is not within the valid range for 8-bit binary
+        return "Overflow"
+        
+    # Empty argument to store binary number
+    binary = ""
+        
+    # For loop for i in a range of 0-7 and goes in reverse order
+    for i in range(7, -1, -1):
+            
+        # if decimal is greater than or equal to 2 to the power of i
+        if decimal >= 2 ** i:
+            # Adds 1 to the binary number
+            binary += "1"
+            # Subtracts 2 to the power of i from the decimal
+            decimal -= 2 ** i
+                # else adds 0 to the binary number
+        else:
+            binary += "0"
+            # Fill the binary number with 0s to make it 8 bits
+            return binary.zfill(8)
+
+
+
 # Creates variable called binary_calculator that takes in 3 arguments: bin1, bin2, and operator
 def binary_calculator(bin1, bin2, operator):
 
@@ -35,49 +80,6 @@ def binary_calculator(bin1, bin2, operator):
     else:
         # Wont take any other operators
         return "Error"
-    
-    # Creates a variable called decimal_to_binary that takes in a decimal number and converts it to binary
-    def decimal_to_binary(decimal):
-
-        # Checks if decimal is smaller than 0 or greater than 255
-        if decimal < 0 or decimal > 255:
-            # Returns error message if decimal is not within the valid range for 8-bit binary
-            return "Overflow"
-        
-        # Empty argument to store binary number
-        binary = ""
-        
-        # For loop for i in a range of 0-7 and goes in reverse order
-        for i in range(7, -1, -1):
-            
-            # if decimal is greater than or equal to 2 to the power of i
-            if decimal >= 2 ** i:
-                # Adds 1 to the binary number
-                binary += "1"
-                # Subtracts 2 to the power of i from the decimal
-                decimal -= 2 ** i
-                # else adds 0 to the binary number
-            else:
-                binary += "0"
-                # Fill the binary number with 0s to make it 8 bits
-                return binary.zfill(8)
-            
-    # Creates a variable called binary_to_decimal that takes in a binary number and converts it to decimal
-    def binary_to_decimal(binary):
-
-        # Checks if length is valid for a 8 bit binary number and if all bits are either 0 or 1
-        if len(binary) != 8 or not all(bit in ['0', '1'] for bit in binary):
-            # Returns error message if binary number is not valid
-            return "Error"
-        # Argument to store decimal number, set as 0
-        decimal = 0
-        
-        # For loop for i in a range of 0-7
-        for i in range(8):
-            # Equation for binary to decimal conversion
-            decimal += int(binary[i]) * (2 ** (7 - i))
-            # Returns decimal number
-            return decimal
 
 # Overflow Check
 # Checks if result is less than 0 or greater than 255
